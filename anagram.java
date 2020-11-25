dd875257210e184a6dd417e3768909e555c157d9
Dipesh1243 <45533938+Dipesh1243@users.noreply.github.com>
2020-11-25 18:11:11 +0000

    Update anagram.java

--- anagram.java
+++ anagram.java
@@ -1,125 +1,112 @@
 import java.io.IOException;
-
-import java.util.Arrays;
-
-import java.util.Collection;
-
-import java.util.HashSet;
-
-import java.util.StringTokenizer;
+import java.sql.Array;
+import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
-
 import org.apache.hadoop.fs.Path;
-
 import org.apache.hadoop.io.IntWritable;
-
 import org.apache.hadoop.io.Text;
-
 import org.apache.hadoop.mapreduce.Job;
-
 import org.apache.hadoop.mapreduce.Mapper;
-
 import org.apache.hadoop.mapreduce.Reducer;
-
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class Anagrammator {
+public class Anagram {
+	//list of stop words
+    public static String[] stopwords = {
+            "'tis", "'twas", "a", "able", "about", "across", "after", "ain't", "all", "almost", "also", "am",
+            "among", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "but", "by", "can", "can't", "cannot",
+            "could", "could've", "couldn't", "dear", "did", "didn't", "do", "does", "doesn't", "don't", "either", "else", "ever", "every",
+            "for", "from", "get", "got", "had", "has", "hasn't", "have", "he", "he'd", "he'll", "he's", "her", "hers", "him", "his", "how", "how'd",
+            "how'll", "how's", "however", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "just", "least",
+            "let", "like", "likely", "may", "me", "might", "might've", "mightn't", "most", "must", "must've", "mustn't", "my", "neither",
+            "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "shan't",
+            "she", "she'd", "she'll", "she's", "should", "should've", "shouldn't", "since", "so", "some", "than", "that", "that'll", "that's",
+            "the", "their", "them", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "tis", "to",
+            "too", "twas", "us", "wants", "was", "wasn't", "we", "we'd", "we'll", "we're", "were", "weren't", "what", "what'd", "what's", "when",
+            "when'd", "when'll", "when's", "where", "where'd", "where'll", "where's", "which", "while", "who", "who'd", "who'll", "who's",
+            "whom", "why", "why'd", "why'll", "why's", "will", "with", "won't", "would", "would've", "wouldn't", "yet", "you", "you'd", "you'll", "you're", "you've", "your"};
 
-    static Collection<Text> anagrams = new HashSet<Text>();
 
-    public static class AnagramMakerMapper extends
+	/*The main function of the Mapper is to read data from an input file string by string and create
+	 a token which can be used to form a "key" in order to group the anagrams, this "key" is passed onto the reducer*/
+	
+    public static class WCMapper extends Mapper<Object, Text, Text, Text> {
 
-            Mapper<Object, Text, Text, Text> {
-
-        public void map(Object key, Text value, Context context)
-
-                throws IOException, InterruptedException {
-
-            StringTokenizer itr = new StringTokenizer(value.toString());
-
-            while (itr.hasMoreTokens()) {
-
-                String word = itr.nextToken();
-
-                char[] arr = word.toCharArray();
-
-                Arrays.sort(arr);
-
-                String wordKey = new String(arr);
-
-                context.write(new Text(wordKey), new Text(word));
-
+		private Text keyword = new Text();
+		private Text anagramword = new Text();
+		
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			
+			//string tokenizer allows us to convert a string into a token, the "value.toString" will return an integer value that represents the string 
+            StringTokenizer token = new StringTokenizer(value.toString());
+			/*the while loop makes use of the hasMoreTokens method and will continue to run if there are more tokens available, 
+			it is the algorithm used to pull out the key and words from the input file*/
+            while (token.hasMoreTokens()) {
+            /*A string "word" is set to return the next token from the StringTokenizer,
+             however all special characters in that string will be removed and the string will be changed to lowercase*/
+                String word = token.nextToken().replaceAll("\\W", "").toLowerCase(); 
+                char[] achar = word.toCharArray();										
+                Arrays.sort(achar);														
+                String wordKey = new String(achar).toLowerCase();								
+                keyword.set(wordKey);
+                anagramword.set(word);
+                context.write(keyword, anagramword);
             }
-
         }
-
     }
 
-    // Does not summarize, but simply collets the list of anagramming words for
+    public static class WCReducer extends Reducer<Text, Text, Text, Text> {
 
-    // a key
+        private Text anagramword = new Text();
+        private Text test = new Text();
+        
+        
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-    public static class AnagramAggregatorReducer extends
-
-            Reducer<Text, Text, Text, Text> {
-
-        public void reduce(Text key, Iterable<Text> values, Context context)
-
-                throws IOException, InterruptedException {
-
-            // Collection<Text> anagrams = new HashSet<Text>();
-
-            String anagram = null;
-
+			HashMap<String, Integer> hmap = new HashMap<String, Integer>();
+			
+            
             for (Text val : values) {
 
-                if (anagram == null) {
-
-                    anagram = val.toString();
-
+        
+                    hmap.add(val.toString());
                     
+            
+            }
+            
+            ArrayList<String> arraylist = new ArrayList<String>(hmap);
 
-                } else {
-
-                    anagram = anagram + ',' + val.toString();
-
+            for (int j = 0; j < stopwords.length; j++) {
+                if (arraylist.contains(stopwords[j])) {
+                    arraylist.remove(stopwords[j]);
                 }
-
-                // anagrams.add(val);
-
             }
 
-            context.write(key, new Text(anagram));
+            Collections.sort(arraylist);
+            anagramword.set(arraylist.toString());
+			test.set(hmap.size() + anagramword);
+			//outputs the anagram if and only if the length of the anagram is greater than 1
+            if (arraylist.size() > 1) {
+                context.write(key, test);
 
+            }
         }
-
     }
 
     public static void main(String[] args) throws Exception {
-
         Configuration conf = new Configuration();
-
-        Job job = Job.getInstance(conf, "word count");
-
-        job.setJarByClass(Anagrammator.class);
-
-        job.setMapperClass(AnagramMakerMapper.class);
-
-        job.setReducerClass(AnagramAggregatorReducer.class);
-
+        Job job = Job.getInstance(conf, "anagram");
+        job.setJarByClass(Anagram.class);
+        job.setMapperClass(WCMapper.class);
+        job.setReducerClass(WCReducer.class);
         job.setOutputKeyClass(Text.class);
-
         job.setOutputValueClass(Text.class);
-
         FileInputFormat.addInputPath(job, new Path(args[0]));
-
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
         System.exit(job.waitForCompletion(true) ? 0 : 1);
 
-    }
 
+    }
 }
